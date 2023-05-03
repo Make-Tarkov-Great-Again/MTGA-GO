de1eb04c61751b6114dffdd0ddd1c23327ee7136
NehaxFr <nehaxfr@proton.me>
2023-05-03 20:56:45 +0200

    yee

--- main.go
+++ main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
+		log.Print("welcome to the pool")
 		fmt.Fprintf(response, "Hello my friend, welcome to the pool")
 	})
 
+	log.Print("pool is open")
 	var err error = http.ListenAndServe(":80", nil)
 	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("pool is closed")
+		log.Print("pool is closed")
 	} else if err != nil {
-		fmt.Printf("someone pissed in the pool: %s", err)
+		log.Fatal(err)
 	}
 }
