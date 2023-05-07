04951578edc98cb8dfb3107f1255a359e442d13d
king <aclengen@gmail.com>
2023-05-07 13:40:08 -0400

    [refactor] TimeInHMSFormat

--- tools/time.go
+++ tools/time.go
@@ -1,13 +1,11 @@
 package tools
 
 import (
-	"fmt"
 	"strconv"
 	"time"
 )
 
 var now = time.Now()
-var pf = fmt.Sprintf
 
 // GetCurrentTimeInSeconds returns the current time in seconds
 func GetCurrentTimeInSeconds() string {
@@ -16,6 +14,5 @@ func GetCurrentTimeInSeconds() string {
 
 // TimeInHMSFormat returns the current time in the format HH-MM-SS
 func TimeInHMSFormat() string {
-	hours, minutes, seconds := now.Second(), now.Minute(), now.Hour()
-	return pf("%02d-%02d-%02d", hours, minutes, seconds)
+	return now.Format("15-04-05")
 }
