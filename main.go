1fb9ea31081ac54a12e923fe6feaeea78055b9b9
king <aclengen@gmail.com>
2023-07-29 09:13:59 -0400

    [add] simple http server

--- main.go
+++ main.go
@@ -1,12 +1,42 @@
+// Package main is a package declaration
 package main
 
 import (
 	"MT-GO/database"
+	"fmt"
+	"net/http"
+	"strconv"
 )
 
 func main() {
 
 	database.InitializeDatabase()
-	//db := database.GetDatabase()
+	db := database.GetDatabase()
 
+	ip := db.Core.ServerConfig.IP
+	port := ":" + strconv.Itoa(db.Core.ServerConfig.Port)
+	address := ip + port
+
+	setHTTPServer(address)
 }
+
+func setHTTPServer(address string) {
+	httpMux := http.NewServeMux()
+	setRoutes(httpMux)
+
+	fmt.Println("Starting HTTP server on " + address)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func setRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("/", homeHandler)
+}
+
+func homeHandler(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func generateCertificate() {}
