d2b488345e7c24ff159f2a5a08831675e53eb254
king <aclengen@gmail.com>
2023-08-31 13:51:17 -0400

    [refact] optimize mongoid & remove tool

--- tools/mongo.go
+++ tools/mongo.go
@@ -8,25 +8,29 @@ import (
 
 const alphabet string = "abcdef0123456789"
 
+var alphabetLength int = len(alphabet)
+
 // taken from https://github.com/matoous/go-nanoid/blob/master/gonanoid.go
 // because i didnt need to use the whole package 8===D
 
+const size int = 24
+
+var chars = []rune(alphabet)
+var length int = len(chars)
+
 // GenerateMongoId returns a random string of length 24
 func GenerateMongoID() (string, error) {
-	chars := []rune(alphabet)
-	size := 24
-
-	if len(alphabet) == 0 || len(alphabet) > 255 {
+	if alphabetLength == 0 || alphabetLength > 255 {
 		return "", errors.New("alphabet must not be empty and contain no more than 255 chars")
 	}
 	if size <= 0 {
 		return "", errors.New("size must be positive integer")
 	}
 
-	mask := getMask(len(chars))
+	mask := getMask(length)
 	// estimate how many random bytes we will need for the ID, we might actually need more but this is tradeoff
 	// between average case and worst case
-	ceilArg := 1.6 * float64(mask*size) / float64(len(alphabet))
+	ceilArg := 1.6 * float64(mask*size) / float64(alphabetLength)
 	step := int(math.Ceil(ceilArg))
 
 	id := make([]rune, size)
@@ -38,7 +42,7 @@ func GenerateMongoID() (string, error) {
 		}
 		for i := 0; i < step; i++ {
 			currByte := bytes[i] & byte(mask)
-			if currByte < byte(len(chars)) {
+			if currByte < byte(length) {
 				id[j] = chars[currByte]
 				j++
 				if j == size {
